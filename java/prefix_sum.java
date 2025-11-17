41677714e88ed652613438263e276ddfde55ec24
Riripa3 <regencypatel7139@gmail.com>
2025-11-17 16:52:39 +0530

    prefixsum-algorithm

---
+++ java/prefix_sum.java
@@ -0,0 +1,8 @@
+/*
+The simple loop takes O(n) EVERY time you ask a query.
+➤ The prefix sum takes O(n) ONLY ONCE.
+
+After that, every query is O(1) — instant.
+
+This is the entire reason prefix sum exists.
+ */
